a527d1b1f840521468fb67d7b9cc364b3f3acffe
pomeyose <149218392+pomeyose@users.noreply.github.com>
2025-01-17 21:13:12 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_97ift1qRY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6urbluh5vcm546dj4433a1m8tm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_EE4xxDa3T', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6sspit6gehi039j3lgagrm5t2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
